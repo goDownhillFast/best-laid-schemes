eaafb0683878fb904e021921250f4d151eeb3fe5
jondayton <jondayton@gmail.com>
2013-07-25 12:58:34 -0400

    basic layout working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,11 @@ require 'google_data'
 class ApplicationController < ActionController::Base
   protect_from_forgery
   helper_method :current_window_activities, :google_data, :current_user
+
   def current_window_activities(window_date_time=nil)
     window_date_time = window_date_time || Time.new
     windows = Window.all
-    windows.reject { |window|  !window.is_right_now?(window_date_time) | !window.activity.plan_for_week.present? }
+    windows.reject { |window| !window.is_right_now?(window_date_time) | !window.activity.plan_for_week.present? }
   end
 
   def google_data
@@ -22,34 +23,48 @@ class ApplicationController < ActionController::Base
     calendar_data = google_data.list_events(opts)
     parsed_calendar_data = convert_google_calendar_data(calendar_data)
     category_totals = {}
-    pp parsed_calendar_data.count
     parsed_calendar_data.each do |item|
-      category_totals[item[:category_name]] ||= {}
-      category_totals[item[:category_name]][:total_time] ||= 0
-      category_totals[item[:category_name]][:total_time] += item[:time]
-      category_totals[item[:category_name]][item[:activity_name]] ||= 0
-      category_totals[item[:category_name]][item[:activity_name]] += item[:time]
+      category_totals[item[:category][:name]] ||= {}
+      category_totals[item[:category][:name]][:total] ||= 0
+      category_totals[item[:category][:name]][:total] += item[:time]
+      category_totals[item[:category][:name]][:activities] ||= {}
+      category_totals[item[:category][:name]][:activities][item[:name]] ||= 0
+      category_totals[item[:category][:name]][:activities][item[:name]] += item[:time]
     end
     category_totals
   end
 
   def convert_google_calendar_data(calendar_data)
-    calendar_data = ActiveSupport::JSON.decode(calendar_data.body)['items']
+    calendar_data = ActiveSupport::JSON.decode(calendar_data.body)['items'].reject! { |item| item['summary'][0..2].to_i == 0 }
     parsed_calendar_data = []
-    all_activities = {}
+
+    activities = all_activities
 
     calendar_data.each do |item|
-      activity_id = item['summary'][0..2].to_i
-      related_activity = Activity.includes(:category).find_by_old_id(activity_id)
+      google_activity_id = item['summary'][0..2].to_i
+      this_activity = activities[google_activity_id]
+
       parsed_calendar_data << {
-          category_id: related_activity.category_id,
-          category_name: related_activity.category.name,
-          activity_id: activity_id,
-          activity_name: related_activity.name,
+          name: this_activity[:name],
+          category: {id: this_activity[:category][:id],
+                     name: this_activity[:category][:name]},
           title: item['summary'],
-          time: (Time.parse(item['end']['dateTime']) - Time.parse(item['start']['dateTime']))/3600} unless activity_id == 0
+          google_event_id: item['id'],
+          time: (Time.parse(item['end']['dateTime']) - Time.parse(item['start']['dateTime']))/3600}
     end
-    parsed_calendar_data.sort_by! {|hash| hash[:category_id]}
+    parsed_calendar_data.sort_by! {|data| data[:category][:id]}
+  end
+
+  def all_activities
+    all_activities = {}
+    Activity.all.each do |activity|
+      all_activities[activity.old_id] ||= {}
+      all_activities[activity.old_id][:name] = activity.name
+      all_activities[activity.old_id][:category] ||= {}
+      all_activities[activity.old_id][:category][:id] = activity.category.id
+      all_activities[activity.old_id][:category][:name] = activity.category.name
+    end
+    all_activities
   end
 
 end
