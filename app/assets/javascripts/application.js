aab071116011ddf39c4862a77f4b55c744417b89
jondayton <jondayton@gmail.com>
2013-08-09 09:56:34 -0400

    Added jquery flot

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,74 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require flot/jquery.flot
 //= require_tree .
 //= require foundation
+
+$(function () {
+  $('.flot-placeholder').each(function (index) {
+    var thisGraph = $(this);
+    var total_time = {color: 'yellow', data: [[parseFloat(thisGraph.attr('data-time-total')), 0]]};
+    if (thisGraph.attr('data-time-budget')) {
+      var budget_time = {
+        color: 'blue', data: [[thisGraph.attr('data-time-budget'), 0]]
+      }
+    } else {
+      var budget_time = {color: 'blue', data: [[thisGraph.attr('data-time-budget'), 0]]}
+    }
+    thisGraph.plot([total_time,
+      budget_time
+    ], defaultGraphOptions).data('plot');
+  });
+});
+
+
+var defaultGraphOptions = {
+  grid: {
+//    show: false,
+    borderWidth: 0
+  },
+  bars: {
+    align: "center",
+    barWidth: 0.5,
+    horizontal: true,
+//    fillColor: { colors: [{ opacity: 0.5 }, { opacity: 1}] },
+    lineWidth: 1
+  },
+  yaxis: {
+    show: false},
+  xaxis: {
+    show: false},
+  series: {
+    bars: {show: true},
+//    lines, points, bars: {
+//      show: boolean
+//      lineWidth: number
+//      fill: boolean or number
+//      fillColor: null or color/gradient
+//    }
+
+//    lines, bars: {
+//      zero: boolean
+//    }
+
+//    points: {
+//      radius: number
+//      symbol: "circle" or function
+//    }
+
+//    bars: {
+//      barWidth: number
+//      align: "left", "right" or "center"
+//          horizontal: boolean
+//}
+//
+//lines: {
+//  steps: boolean
+//}
+
+    shadowSize: 0
+//highlightColor: color or number
+  }
+//  colors: []
+};
