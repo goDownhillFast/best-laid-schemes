f9faa101a83d17ad96d75dcf2de201004783af62
jondayton <jondayton@gmail.com>
2013-08-12 13:19:10 -0400

    added google_data lib back

---
+++ lib/google_data.rb
@@ -0,0 +1,42 @@
+require 'pp'
+class GoogleData
+
+  def initialize(token)
+    @client = Google::APIClient.new
+    @client.authorization.access_token = token
+    @calendar = @client.discovered_api('calendar', 'v3')
+  end
+
+  def get_time_zone
+    ActiveSupport::JSON.decode(@client.execute(api_method: @calendar.settings.get, :parameters => {'setting' => 'timezone'}).response.env[:body])['value']
+  end
+
+  def list_events(opts={})
+    opts[:calendarId] ||= 'primary'
+    @client.execute(api_method: @calendar.events.list,
+                    parameters: opts)
+  end
+
+  def calendar_list
+    @client.execute(
+        :api_method => @calendar.calendar_list.list,
+        :parameters => {},
+        :headers => {'Content-Type' => 'application/json'})
+  end
+
+  def new_event(calendar_id="primary", opts={})
+    @client.execute(api_method: @calendar.events.insert,
+                    parameters: {calendarId: calendar_id},
+                    body: JSON.dump({summary: opts[:title],
+                                     location: opts[:location],
+                                     start: {
+                                         dateTime: opts[:start_time]
+                                     },
+                                     end: {
+                                         dateTime: opts[:end_time]
+                                     }
+                                    }),
+                    headers: {'Content-Type' => 'application/json'})
+
+  end
+end
