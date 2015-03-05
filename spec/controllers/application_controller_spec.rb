d2315982d3e7f9685929a2d4814c7ac247532b09
jondayton <jondayton@gmail.com>
2015-03-04 23:49:48 -0500

    streamline application controller test

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -2,12 +2,12 @@ require 'spec_helper'
 
 describe ApplicationController do
   describe "#current_user" do
-    before do
-      @user = FactoryGirl.create(:user)
-      allow(controller).to receive(:session).and_return( {email: @user.email} )
-    end
+    let(:user) { create(:user) }
+
+    before { allow(controller).to receive(:session).and_return( {email: user.email} ) }
+
     it "finds an existing user" do
-      expect(controller.current_user.id).to eq(@user.id)
+      expect(controller.current_user.id).to eq(user.id)
     end
   end
 end
