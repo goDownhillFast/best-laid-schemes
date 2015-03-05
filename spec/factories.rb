3162977f63c6f208f37186b340dbb1908364d1bf
jondayton <jondayton@gmail.com>
2015-03-04 23:50:04 -0500

    factory for categories

--- spec/factories.rb
+++ spec/factories.rb
@@ -3,5 +3,16 @@ FactoryGirl.define do
     first_name "Johnny"
     last_name "Planner"
     sequence(:email) {|n| "plannerboy#{n}@zzzzzz.com"}
+
+    factory :user_with_categories do
+      after(:create) do |user|
+        5.times {create(:category, user: user)}
+      end
+    end
+  end
+
+  factory :category do
+    sequence(:name) {|n| "category#{n}"}
+    sequence(:budget) {|n| (n + 1) / 2}
   end
 end
