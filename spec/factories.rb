ff4fe8233286415d9b67aa0ff6e408216996bcc1
jondayton <jondayton@gmail.com>
2015-03-12 23:39:41 -0400

    update factories with categories and activities

--- spec/factories.rb
+++ spec/factories.rb
@@ -9,10 +9,33 @@ FactoryGirl.define do
         5.times {create(:category, user: user)}
       end
     end
+
+    factory :user_with_categories_and_activities do
+      after(:create) do |user|
+        5.times {create(:category_with_activities, user: user, planning_user: user)}
+      end
+    end
   end
 
   factory :category do
+    ignore do
+      planning_user nil
+    end
+
     sequence(:name) {|n| "category#{n}"}
     sequence(:budget) {|n| (n + 1) / 2}
+
+    factory :category_with_activities do
+      after(:create) do |category|
+        5.times {create(:activity, category: category, user: planning_user)}
+      end
+    end
   end
+
+  factory :activity do
+    sequence(:name) {|n| "activity#{n}"}
+    repeating true
+    sequence(:budget) {|n| n}
+  end
+
 end
