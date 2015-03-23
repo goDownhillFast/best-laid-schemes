1f135aa41fcc964439694798f1813c7b27eeddd6
jondayton <jondayton@gmail.com>
2015-03-23 00:21:18 -0400

    totals and display tweaks

--- app/controllers/budget_controller.rb
+++ app/controllers/budget_controller.rb
@@ -2,11 +2,11 @@ require 'active_support/all'
 class BudgetController < ApplicationController
 
   before_filter :events
-  helper_method :event_day_activity_total
+  helper_method :time_by_activity, :day_name
 
   def index
-    @this_week_to_date = events.where({time_min: beginning_of_week,
-                                          time_max: right_now})
+    @this_week = events.where({time_min: beginning_of_week,
+                                          time_max: beginning_of_week + 7.days})
 
     @last_week = events.where({time_min: beginning_of_week - 7.days,
                                               time_max: beginning_of_week})
@@ -14,10 +14,17 @@ class BudgetController < ApplicationController
     @categories = current_user.categories.includes(:activities).all
   end
 
-  def event_day_activity_total(old_id, i)
-    selected_events = @this_week_to_date.select do |event|
-      event[:old_activity_id] === old_id && event[:start].wday === i
+  def time_by_activity(event_set, old_activity_id, i = nil)
+    if i
+      event_set.select! do |event|
+        event[:old_activity_id] === old_activity_id && event[:start].wday === i
+      end
     end
-    selected_events.map { |e| e[:time] }.reduce(:+)
+
+    event_set.map { |e| e[:time] }.reduce(:+)
+  end
+
+  def day_name(i)
+    (beginning_of_week + i.days).strftime('%a')
   end
 end
